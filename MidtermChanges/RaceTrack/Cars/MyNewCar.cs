f877cbb974801473b4fde1e8f69a3a28ee7460df
Josh Stueck <100880735+jstueck3@users.noreply.github.com>
2023-10-12 19:03:01 -0500

    Add files via upload

---
+++ MidtermChanges/RaceTrack/Cars/MyNewCar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class MyNewCar : RaceCar
+    {
+        public MyNewCar(string name, int topSpeed)
+        {
+            Name = name;
+            TopSpeed = topSpeed;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"Starting the engine of my custom car, {Name}!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"My custom car, {Name}, comes to a screeching halt!");
+        }
+
+    }
+}
